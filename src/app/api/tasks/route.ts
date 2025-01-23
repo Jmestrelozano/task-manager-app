0e8066a41e9860e3baf8e35471ef0734e058292b
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-22 19:03:00 -0500

    🦄 refactor: enhance task creation with additional properties
    - Updated `POST` handler to support new task properties:
      - Added `stage`, `priority`, and `date` fields to tasks.
      - Introduced `createdAt` and `updatedAt` timestamps for better tracking.
    - Changed task ID from `id` to `_id` for consistency with the database structure.
    - Ensured the `GET` handler remains unchanged, returning all tasks.

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -2,22 +2,26 @@ import { NextResponse } from "next/server";
 import {db} from "../db/db"
 
 export async function GET(request: Request) {
-  const tasks = db.getTasks(); // Obtener tareas actuales
+  const tasks = db.getTasks();
   return NextResponse.json(tasks, { status: 200 });
 }
 
 export async function POST(request: Request, res: Response) {
   const body = await request.json();
-  const { title, description, status } = body;
+  const { title, description, stage, priority, date } = body;
 
   const tasks = db.getTasks();
   const newTask = {
-    id: (tasks.length + 1).toString(),
+    _id: (tasks.length + 1).toString(),
     title,
     description,
-    status,
+    stage,
+    priority,
+    date,
+    createdAt: new Date().toISOString(),
+    updatedAt:  new Date().toISOString(),
   };
 
-  db.addTask(newTask); // Agregar nueva tarea
+  db.addTask(newTask);
   return NextResponse.json(newTask, { status: 201 });
 }
