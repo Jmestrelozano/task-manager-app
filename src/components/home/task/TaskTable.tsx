3d4d248878d070afc81e21923bf40b4fe5770955
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-22 19:16:44 -0500

    ✨ feat: add TaskTable with edit and delete functionality
    - Implemented `TaskTable` component to display tasks in a table format.
    - Added `AddTaskModal` for editing tasks and `ConfirmationModal` for task deletion.
    - Included functions to handle opening modals for editing and confirming task deletion.
    - Integrated `useDeleteTask` hook to delete tasks.
    - Updated table rows to allow for editing and deleting individual tasks.

---
+++ src/components/home/task/TaskTable.tsx
@@ -0,0 +1,65 @@
+import React, { FC, Fragment, useState } from "react";
+import { useDeleteTask } from "@/hooks";
+import { AddTaskModal } from "./AddTaskModal";
+import { ConfirmationModal } from "@/components/common/modals/ConfirmationModal";
+import { TaskTableHeader } from "./TaskTableHeader";
+import { TaskTableRow } from "./TaskTableRow";
+import { ITable } from "@/interfaces";
+
+export const TaskTable: FC<ITable> = ({ tasks }) => {
+  const { mutate: deleteTask } = useDeleteTask();
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [selectedTaskId, setSelectedTaskId] = useState<string>("");
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const handleOpenDeleteModal = (taskId: string) => {
+    setSelectedTaskId(taskId);
+    setIsDeleteDialogOpen(true);
+  };
+
+  const handleOpenEditModal = (taskId: string) => {
+    setIsEditModalOpen(true);
+    setSelectedTaskId(taskId);
+  };
+
+  const confirmDeleteTask = () => {
+    deleteTask(selectedTaskId);
+    setIsDeleteDialogOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <div className="bg-white px-2 md:px-4 pt-4 pb-9 shadow-md rounded">
+        <div className="overflow-x-auto">
+          <table className="w-full">
+            <TaskTableHeader />
+            <tbody>
+              {tasks.map((task) => (
+                <TaskTableRow
+                  openDeleteModal={handleOpenDeleteModal}
+                  openEditModal={handleOpenEditModal}
+                  key={task._id}
+                  task={task}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <AddTaskModal
+        isOpen={isEditModalOpen}
+        setIsOpen={setIsEditModalOpen}
+        task={tasks.find(({ _id }) => _id === selectedTaskId)}
+        key={new Date().getTime()}
+      />
+
+      <ConfirmationModal
+        title="Are you sure you want to delete the selected record?"
+        isOpen={isDeleteDialogOpen}
+        setIsOpen={setIsDeleteDialogOpen}
+        onClick={confirmDeleteTask}
+      />
+    </Fragment>
+  );
+};
