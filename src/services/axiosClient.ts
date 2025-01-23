0df2372d9b755f8944bb742d555f9bdfc3a5444e
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-23 01:39:59 -0500

    🦄 refactor: Update axios client to use environment variable for API URL

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api', // Cambia por tu URL base
-  timeout: 10000, // Tiempo de espera en ms
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Puedes agregar interceptores si es necesario
 axiosClient.interceptors.request.use((config) => {
-  // Por ejemplo, agregar un token
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -21,7 +19,6 @@ axiosClient.interceptors.request.use((config) => {
 axiosClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    // Manejo de errores
     console.error('Error en la respuesta:', error);
     return Promise.reject(error);
   }
