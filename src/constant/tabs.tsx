e392767a0a56db504533bf127481e8eddba9d38a
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-22 19:21:54 -0500

    ✨ feat: add tabs for board and list view
    - Added two tabs for task views: "Board View" and "List View" with corresponding icons `MdGridView` and `FaList`.
    - These tabs allow toggling between different task view layouts.

---
+++ src/constant/tabs.tsx
@@ -0,0 +1,7 @@
+import { FaList } from "react-icons/fa";
+import { MdGridView } from "react-icons/md";
+
+export const TABS = [
+  { title: "Board View", icon: <MdGridView /> },
+  { title: "List View", icon: <FaList /> },
+];
