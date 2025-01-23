7531bba15dece89e3a165c1617fb0726874e0032
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-22 19:23:46 -0500

    ✨ feat: create custom hook for fetching tasks
    - Added `useTasks` hook to manage fetching tasks and updating the store.
    - Handles loading and error states during task fetch.

---
+++ src/hooks/useTasks.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { getTasks } from "../services/tasks/getTasks";
+import { useTaskStore } from "@/store/useTaskStore";
+
+interface UseTasksResult {
+  fetchTasks: () => Promise<void>;
+  isLoading: boolean;
+  error: string | null;
+}
+
+export const useTasks = (): UseTasksResult => {
+  const setTasks = useTaskStore((state) => state.setTasks);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const tasks = await getTasks();
+      setTasks(tasks);
+    } catch (err: any) {
+      setError(err.message || "An error occurred while fetching tasks.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { fetchTasks, isLoading, error };
+};
