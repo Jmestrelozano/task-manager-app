f9911e7d12b9f4a7c9da79b403e1510018aaf7a0
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-22 19:04:09 -0500

    🦄 refactor: update task management API for flexibility and consistency
    - Refactored `PUT` handler:
      - Extracted task ID directly from the request URL for improved flexibility.
      - Updated tasks using the new `updateTask` method, allowing partial updates with dynamic fields.
    - Updated `DELETE` handler:
      - Extracted task ID directly from the request URL for consistency.
      - Modified response to return the updated task list after deletion.
    - Removed `context.params` usage to simplify parameter handling and improve compatibility.

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -5,33 +5,38 @@ interface IParams {
   id?: string;
 }
 
-export async function PUT(request: Request, context: { params: IParams }) {
-  const { id } = context.params;
-  const body = await request.json();
-  const { status } = body;
-
+export async function PUT(request: Request) {
+  const id = request.url.split("/").pop();
   const taskIndex = db.findTaskIndex(id!);
 
   if (taskIndex === -1) {
-    return NextResponse.json({ message: "Tarea no encontrada" }, { status: 404 });
+    return NextResponse.json(
+      { message: "Tarea no encontrada" },
+      { status: 404 }
+    );
   }
 
-  db.updateTaskStatus(taskIndex, status);
+  const body = await request.json();
+
+  db.updateTask(taskIndex, body);
   const updatedTask = db.getTasks()[taskIndex];
 
   return NextResponse.json(updatedTask, { status: 200 });
 }
 
 export async function DELETE(request: Request, context: { params: IParams }) {
-  const { id } = context.params;
+  const id = request.url.split("/").pop();
 
   const taskIndex = db.findTaskIndex(id!);
 
   if (taskIndex === -1) {
-    return NextResponse.json({ message: "Tarea no encontrada" }, { status: 404 });
+    return NextResponse.json(
+      { message: "Tarea no encontrada" },
+      { status: 404 }
+    );
   }
 
   db.deleteTask(taskIndex);
 
-  return NextResponse.json({ message: "Tarea eliminada" }, { status: 200 });
+  return NextResponse.json(db.getTasks(), { status: 200 });
 }
