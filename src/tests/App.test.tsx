3100dfadc9674bb468c3abe0e6de0ddedf394577
Jmestrelozano <jmestrelozano@gmail.com>
2025-01-23 01:03:52 -0500

    🧪 test: add basic test to render Home component
    - Added a test to render the Home component using @testing-library/react.
    - Verified that the component renders without errors.

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { render, screen  } from '@testing-library/react';
-import { describe, it } from 'vitest'
+import { render  } from '@testing-library/react';
 import Home from '@/app/page';
 
 describe('App', () => {
   it('renders the App component', () => {
     render(<Home />)
-    
-    screen.debug(); // prints out the jsx in the App component unto the command line
   })
 })
